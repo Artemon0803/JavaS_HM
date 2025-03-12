afcedcc04c3ea45eab3773b3e95a0865055f4300
Artemon0803 <artempcela21@gmail.com>
2025-03-12 19:54:01 +0200

    Commit changes

--- src/hm2/LogicOpTableV2.java
+++ src/hm2/LogicOpTableV2.java
@@ -1,7 +1,25 @@
 package hm2;
 
 public class LogicOpTableV2 {
+    static int convert(boolean val){
+        return val ? 1 : 0;
+    }
+    static void print(boolean p, boolean q){
+        System.out.print(convert(p) + "\t" + convert(q) +"\t");
+        System.out.print(convert(p&q) + "\t" + convert(p|q) + "\t");
+        System.out.println(convert(p^q) + "\t" + convert(!p));
+    }
     public static void main(String[] args) {
+        //Compact version
+        System.out.println("P\tQ\tAND\tOR\tXOR\tNOT");
+        print(true, true);
+        print(true, false);
+        print(false, true);
+        print(false, false);
+
+
+        //Full version
+        /*
         boolean p, q;
 
         System.out.println("P\tQ\tAND\tOR\tXOR\tNOT");
@@ -65,5 +83,6 @@ public class LogicOpTableV2 {
         else System.out.print("0" + "\t");
         if((!p)) System.out.println("1");
         else System.out.println("0");
+         */
     }
 }
